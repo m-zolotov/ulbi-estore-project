1910fe4458dcc579b6c64ab3ce0a497135a8d4bb
Mikhail Zolotov <m.v.zolotov@gmail.com>
2024-11-06 03:11:45 +0300

    Connecting React and configuring CSS in webpack

---
+++ src/components/Text.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Text.scss'
+
+export const Text = () => {
+    return (
+        <div>Hello, World!</div>
+    )
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { render } from 'react-dom';
+import { Text } from './components/Text';
+
+render(
+    <div>
+        <Text />
+    </div>,
+    document.getElementById('root')
+)
