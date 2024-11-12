98e579b7352b5e55ef51adaa1059245392da5103
Mikhail Zolotov <m.v.zolotov@gmail.com>
2024-11-13 01:43:41 +0300

    Add MiniCssExtractPlugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,15 @@
 import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader",
+            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
         ],
     }
 
