45b4a5da7e39a6f65d467c6ff727b091ba27223e
Mikhail Zolotov <m.v.zolotov@gmail.com>
2024-11-20 23:54:28 +0300

    Add router, code splitting, lazy, suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Text } from "./components/Text";
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import './index.scss';
 
 export const App = () => {
     return (
         <div className="app">
-            Welcome!
-            <Text />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О компании</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
